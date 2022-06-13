d15dcee9c4f6e0293b3fbaf28b9b1187fffc8f3d
mark127DARK <84608380+mark127DARK@users.noreply.github.com>
2022-06-13 14:12:48 +0800

    Update settings.py

--- OnlineQuizPlatform/settings.py
+++ OnlineQuizPlatform/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'OnlineQuizPlatform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbquizplatform',
+        'USER': 'postgres',
+        'PASSWORD': 'user123',
+        'PORT': '5432',
+        'HOST': '127.0.0.1',
     }
 }
 
